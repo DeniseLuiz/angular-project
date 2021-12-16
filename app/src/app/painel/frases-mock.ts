08de6860940ef257214f2e04b3a61b72d9a5998f
DeniseLuiz <denisesantos.luiz@usp.br>
2021-12-15 23:40:29 -0300

    tentativas

---
+++ app/src/app/painel/frases-mock.ts
@@ -0,0 +1,20 @@
+import { Frase } from "../shared/frase.model";
+
+export const FRASES: Array<Frase> = [
+  {
+    fraseEnglish: 'I like to learn',
+    frasePtBr: 'Eu gosto de aprender'
+  },
+  {
+    fraseEnglish: 'I watch tv',
+    frasePtBr: 'Eu assisto tv'
+  },
+  {
+    fraseEnglish: 'How are you?',
+    frasePtBr: 'Como vai você?'
+  },
+  {
+    fraseEnglish: 'I eat bread',
+    frasePtBr: 'Eu como pão'
+  }
+]
--- app/src/app/painel/painel.component.ts
+++ app/src/app/painel/painel.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { FRASES } from './frases-mock';
+import { Component, EventEmitter, OnInit, Output,} from '@angular/core';
+import { Frase } from '../shared/frase.model';
 
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit{
 
-  constructor() { }
+  public frases: Array<Frase> = FRASES;
+  public instrucao: string = 'Traduza a frase:';
+  public resposta: string = '';
+  public rodada:number = 0;
+  public rodadaFrase: Frase = {fraseEnglish: '', frasePtBr: ''};
+  public progresso: number = 0;
+  public tentativas: number = 3;
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter() //LEMVBRAR DO IGUAL
+
+  constructor() {
+    this.atualizaRodada();
+  }
 
   ngOnInit(): void {
   }
 
+  public atualizaResposta(resposta: Event){
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  public verificarResposta(){
+    if(this.resposta == this.rodadaFrase.frasePtBr){
+      this.rodada++;
+      this.progresso = this.progresso + (100/ this.frases.length);
+
+      if(this.rodada === 4){
+        alert('Parabéns! Você concluiu as traduções com sucesso!');
+        this.encerrarJogo.emit('vitoria');
+      }
+      this.atualizaRodada();
+    }else{
+      alert('A tradução está incorreta');
+      this.tentativas--;
+      if(this.tentativas === -1){
+        alert('Você perdeu todas as tentivas');
+        this.encerrarJogo.emit('derrota');
+      }
+    }
+  }
+
+  public atualizaRodada(){
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = '';
+  }
 }
